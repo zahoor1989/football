0b991183b0692c38477de7c9106a39d23e7d747f
Zahoor Ahmed <zdev1989@gmail.com>
2023-09-06 17:20:07 +0200

    Deplyment changes

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'xtreme-admin-angular-lite'`, () => {
+  it(`should have as title 'youth-league-management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('xtreme-admin-angular-lite');
+    expect(app.title).toEqual('youth-league-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('xtreme-admin-angular-lite app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('youth-league-management app is running!');
   });
 });
